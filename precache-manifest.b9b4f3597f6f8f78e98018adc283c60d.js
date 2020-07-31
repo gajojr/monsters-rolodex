007dc3f43e2ab4f7b9e42cbcdecbeda85251a46c
Andrija <andreccccc12@gmail.com>
2020-07-31 17:54:58 +0200

    Updates

---
+++ precache-manifest.b9b4f3597f6f8f78e98018adc283c60d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf283b86b8f65116d2f07aadd893a577",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "211acb509a40b20f26f7",
+    "url": "/monsters-rolodex/static/css/main.ee57810e.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "211acb509a40b20f26f7",
+    "url": "/monsters-rolodex/static/js/main.430b1128.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
